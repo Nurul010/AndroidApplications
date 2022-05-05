c0c8653d18ebc31a8479713d813b4ed2c9c783fe
Nurul010 <104237074+Nurul010@users.noreply.github.com>
2022-05-05 02:18:52 -0500

    Update Inheritance.kt

--- KotlinBasics3/app/src/main/java/com/example/helloworld/Inheritance.kt
+++ KotlinBasics3/app/src/main/java/com/example/helloworld/Inheritance.kt
@@ -6,19 +6,24 @@ package com.example.helloworld
 // inherited is called the Super class or Parent class
 // or Base class.
 
-//super class, parent class, base class
-open class Vehicle(){ //need to declare open for other class to inherit
-    // properties
-    // methods
+interface Drivable{ // useful for declaring later functions and variables
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
 }
 
-// Sub Class Child class or Derived class of Vehicle,
 // Super Class, Parent Class, Base Class of Vehicle
-open class car1(val name: String, val brand: String){
+open class car1(val name: String, val brand: String, override val maxSpeed: Double): Drivable{//need to declare open for other class to inherit
     open var range: Double = 0.0 // needs to be open for override in child class
     fun extendRange(amount: Double){
         if(amount > 0)
             range += amount
+    }
+    //override fun drive(): String = "driving the interface drive" -> same thing as below:
+    override fun drive(): String {
+        return "driving the interface drive"
     }
     open fun drive(distance: Double){
         println("Drove for $distance KM")
@@ -26,22 +31,37 @@ open class car1(val name: String, val brand: String){
 }
 
 // Sub Class Child class or Derived class of Car class,
-class ElectricCar(name: String, brand: String, batteryLife: Double) : car1(name, brand){ // needs the same constructor as parent
+class ElectricCar(name: String, brand: String, batteryLife: Double, maxSpeed: Double) : car1(name, brand,
+    maxSpeed
+){ // needs the same constructor as parent
     override var range = batteryLife * 5
     override fun drive( distance: Double){
         println("Drove for $distance Km on electricity")
     }
 
-    fun drive(){
-        println("Drove for $range Km on electricity")
+    //Cannot be used anymore since it is trying to override drive from inheritance in car:
+//    fun drive(){
+//        println("Drove for $range Km on electricity")
+//    }
+
+    override fun drive(): String{
+        return "Drove for $range Km on electricity"
+    }
+
+    override fun brake(){
+        //super.brake() // car did not call it so we need to call from superclass
+        println("brake inside of electricity car")
     }
 }
 
 fun main(){
-    var AudiA3 = car1("A3", "Audi")
-    var TeslaS = ElectricCar("s-Model", "Tesla", 85.0)
+    var AudiA3 = car1("A3", "Audi", 200.0)
+    var TeslaS = ElectricCar("s-Model", "Tesla", 85.0, 200.0)
     TeslaS.extendRange(200.0)
     AudiA3.drive(200.0)
     TeslaS.drive(195.0)
-    TeslaS.drive()
+    //TeslaS.drive()
+    println(TeslaS.drive())
+    TeslaS.brake()
+    AudiA3.brake()
 }
